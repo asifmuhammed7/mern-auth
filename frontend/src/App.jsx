91232d659ef88902507679938c10283e0c5c4e7f
asif <asifmuhammedn7@gmail.com>
2024-01-08 12:05:02 +0530

    created screens and created forms

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
+import { Outlet } from "react-router-dom";
+import { Container } from "react-bootstrap";
 import Header from "./components/Header";
-
+import HomeScreen from "./screens/HomeScreen";
 const App = ()=>{
   return (
     <>
     <Header/>
-      <h1>My App</h1>
+    <Container>
+      <Outlet/>
+    </Container>
+    
     </>
   )
 };
